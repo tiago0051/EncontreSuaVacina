3f1dd555a7ab6a8ceb2b8a68f76f34ea5a283116
Tiago Salgado <tiagosalgado2002@hotmail.com>
2021-10-27 14:20:36 -0300

    UPDATE LAYOUT

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,13 @@ export default function Home(props) {
         <title>Localize sua Vacina</title>
         <meta name="description" content="Localize o ponto de distribuição mais proximo de você!" />
       </Head>
+
+      <form>
+        <h2>Descubra o local ideal para tomar sua vacina!</h2>
+        <input type="text" placeholder="Digite a localização"/>
+        <input type="number" placeholder="Idade"/>
+        <button type="submit">Pesquisar</button>
+      </form>
     </div>
   )
 }
