befb3e711131d2b493dc4d8f0dde93a881a597b8
Tiago Salgado <tiagosalgado2002@hotmail.com>
2021-12-08 08:27:51 -0300

    Atualização do metodo de inserção das informações na pagina resultado

--- pages/resultado.js
+++ pages/resultado.js
@@ -1,33 +1,22 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 import Router from 'next/router'
 import absoluteUrl from "next-absolute-url"
 
 import styles from '../styles/Home.module.css';
 
 export default function Resultado(props) {
-
-    const [Cidade, setCidade] = useState('');
-    const [Idade , setIdade] = useState(0);
-    const [Postos, setPostos] = useState([])
-
-    useEffect(() => {
-        const {cidade, idade, postos} = props;
-        setCidade(cidade);
-        setIdade(idade);
-        setPostos(postos)
-    }, [])
+    const {cidade, idade, postos} = props;
 
     return (
         <div className={styles.container}>
             <main className={styles.resultado}>
                 <div>                   
-                    <h1 className={styles.title}>Resultado para a cidade de {Cidade} para {Idade} anos.</h1>
+                    <h1 className={styles.title}>Resultado para a cidade de {cidade} para {idade} anos.</h1>
                     <button onClick={() => Router.push("/")} className={styles.botões}>Pesquisar Novamente</button>
                 </div>
                 <div>
                     {
-                        Postos.map(posto => (
+                        postos.map(posto => (
                             <span key={posto}>{posto}</span>
                         ))
                     }
